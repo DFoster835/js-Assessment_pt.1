80c031debe24f9846af450e9ae326082828e2a4a
DFoster835 <78561775+DFoster835@users.noreply.github.com>
2022-04-30 15:00:45 -0500

    Create bestPractice.js

---
+++ bestPractice.js
@@ -0,0 +1,19 @@
+exports = typeof window === 'undefined' ? global : window;
+
+exports.bestPracticesAnswers = {
+  globals: function() {
+    let myObject = {
+      name: 'Jory'
+    };
+
+    return myObject;
+  },
+
+  parseInt: function(num) {
+    return parseInt(num, 10);
+  },
+
+  identity: function(val1, val2) {
+    return val1 === val2;
+  }
+};
